da3dde14e2904ee939a9f803edbc7a81a23ae066
amh6281 <amh6281@naver.com>
2024-02-18 09:14:53 +0900

    Avatar style 개선

--- components/Live.tsx
+++ components/Live.tsx
@@ -154,7 +154,7 @@ const Live = () => {
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
-      className="h-[100vh] w-full flex justify-center items-center text-center border-2 border-green-500"
+      className="h-[100vh] w-full flex justify-center items-center text-center"
     >
       <h1>hello</h1>
 
